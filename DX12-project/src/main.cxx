deb59938bb3faafd272fa7c55d8da71cfb430af1
Almaz Kinziabulatov <auqolaq@gmail.com>
2019-11-19 20:56:22 +0200

    Created DXGI factory and obtained adapters.

--- DX12-project/src/main.cxx
+++ DX12-project/src/main.cxx
@@ -1,9 +1,50 @@
 #include "main.hxx"
 
+#pragma comment(lib, "DXGI.lib")
 
 int main()
 {
 #if defined(_DEBUG) || defined(DEBUG)
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
+
+    IDXGIFactory *dxgi_factory = nullptr;
+
+    if (auto result = CreateDXGIFactory1(IID_PPV_ARGS(&dxgi_factory)); result != S_OK)
+        throw std::runtime_error(fmt::format("failed to create DXGI factory instance: {0:#x}"s, result));
+
+    std::vector<IDXGIAdapter *> adapters(16);
+
+    std::generate(std::begin(adapters), std::end(adapters), [dxgi_factory, i = 0u] () mutable
+    {
+        IDXGIAdapter *adapter = nullptr;
+
+        dxgi_factory->EnumAdapters(i++, &adapter);
+
+        return adapter;
+    });
+
+    std::sort(std::begin(adapters), std::end(adapters));
+
+    {
+        auto it = std::remove_if(std::begin(adapters), std::end(adapters), [] (auto adapter) { return adapter == nullptr; });
+
+        adapters.erase(it, std::end(adapters));
+    }
+
+    for (auto adapter : adapters) {
+        DXGI_ADAPTER_DESC description;
+        adapter->GetDesc(&description);
+
+        std::wcout << L"Adapter "s + description.Description << std::endl;
+    }
+
+    for (auto adapter : adapters)
+        adapter->Release();
+
+    //if (auto result = D3D12CreateDevice(); )
+
+    dxgi_factory->Release();
+
+    std::cin.get();
 }
--- DX12-project/src/main.hxx
+++ DX12-project/src/main.hxx
@@ -7,9 +7,20 @@
     #include <crtdbg.h>
 #endif
 
+#include <algorithm>
 #include <execution>
 #include <exception>
+#include <iostream>
+#include <vector>
+
+#include <string>
+using namespace std::string_literals;
+
+#include <string_view>
+using namespace std::string_view_literals;
 
 #include <fmt/format.h>
 
 #include <wrl.h>
+#include <dxgi.h>
+#include <d3d12.h>
